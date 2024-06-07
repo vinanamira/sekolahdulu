14dd021b6b505ee6d13fb576429b2174e3950a65
vinanamira <mzssdaru@gmail.com>
2024-06-07 18:29:22 +0700

    Lock device orientation on portrait

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,21 @@
 import 'package:assesment2/login.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 
 void main() {
-  runApp(const MyApp());
+  // Pastiin semua widget flutter initialized
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Set orientasi layar agar portrait (up / down)
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]).then((value) => runApp(const MyApp()));
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
